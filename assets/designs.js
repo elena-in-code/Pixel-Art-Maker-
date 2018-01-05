30333a1779217dd5c986e8b5ea5a17e3f2fb6586
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-01-05 12:13:09 +0100

    targeting size input values

--- assets/designs.js
+++ assets/designs.js
@@ -1,10 +1,21 @@
-// Select color input
-// Select size input
+// Select color input***
 
+// Select size input***
+const inputHeight = $("#input_height");
+const inputWidth = $("#input_width");
 // When size is submitted by the user, call makeGrid()
+//Event on click of the submit button that will call the makeGrid function:
+$("#input_submit").click(function(evt) {
+  evt.preventDefault();
+  makeGrid();
+});
 
 function makeGrid() {
+	//Get values of size the inputs:
+	var inputHeightValue = inputHeight.val();
+	var inputWidthValue = inputWidth.val();
 
-// Your code goes here!
+	//loop for rows Height:
 
+	//loop for rows Width:
 }
